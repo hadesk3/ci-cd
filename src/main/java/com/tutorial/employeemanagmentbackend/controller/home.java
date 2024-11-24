a628d35f02b4e405a79e15ff5e2cc5fb35b5a722
unknown <phamha2003j@gmail.com>
2024-11-24 08:20:18 +0700

    check ci-cd

--- src/main/java/com/tutorial/employeemanagmentbackend/controller/home.java
+++ src/main/java/com/tutorial/employeemanagmentbackend/controller/home.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/check")
 public class home {
 	public String homepage()
 	{
-		return "hello";
+		return "check ci/cd";
 	}
 }
