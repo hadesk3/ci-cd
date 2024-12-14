2525a60bf3d708750e35d8046af31f24757fa204
unknown <phamha2003j@gmail.com>
2024-12-14 13:54:45 +0700

    test

--- src/main/java/com/tutorial/employeemanagmentbackend/controller/home.java
+++ src/main/java/com/tutorial/employeemanagmentbackend/controller/home.java
@@ -1,11 +1,13 @@
 package com.tutorial.employeemanagmentbackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/check")
 public class home {
+	@GetMapping
 	public String homepage()
 	{
 		return "check ci/cd";
